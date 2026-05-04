7e7840cb21f79ac50bff0134a9c85fc44ff5271c
Tejas Saraf <tejas@thekiranacademy.com>
2026-05-04 12:01:04 +0530

    day07

---
+++ day07.js
@@ -0,0 +1,25 @@
+console.log("hello day 07 js");
+
+function check() {
+  let uname = document.getElementById("un").value;
+
+  let pass = document.querySelector("#pass").value; //
+
+  let message = document.getElementById("msg");
+
+  console.log(uname);
+  console.log(pass);
+
+  if (uname == "tejas" && pass == "tka123") {
+    console.log("login success..");
+    message.innerHTML = "LOGIN SUCCESS..";
+    message.style.color = "blue" ; 
+} else {
+    console.log("login failed..");
+    message.innerHTML = "LOGIN FAILED..";
+    message.style.color = "red" ; 
+  
+  }
+
+  console.log("button is working...");
+}
